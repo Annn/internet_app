7e36692c9890ab9ede7b33cdfab71fcade781c46
annn <avasyutina@gmail.com>
2018-05-23 13:54:05 +0200

    small changes to model request.js, views

--- models/request.js
+++ models/request.js
@@ -4,15 +4,29 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var Request = new Schema({
-    name: {
-        type: String
+    date: {
+        type: Date
     },
-    price: {
+    description: {
+        type: String,
+        required: true
+    },
+    weight: {
         type: Number
+    },
+    place: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: Boolean,
+        required: true
     }
 },
                       {
-    collection: 'reqs'
+    collection: 'requests'
 });
+
+// The first argument is the singular name of the collection your model is for. Mongoose automatically looks for the plural version of your model name.
 
 module.exports = mongoose.model('request', Request);
