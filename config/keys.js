7439174648780e7f5f19a263b96f90295bbf1315
annn <avasyutina@gmail.com>
2018-06-14 08:47:52 +0200

    Improved views

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// add this file to .gitignore
+
+module.exports = {
+    google: {
+        clientID: '426456858811-pciio711jnq7lcppp323sqn2umf0ok8n.apps.googleusercontent.com',
+        clientSecret: 'jtflmI1130m4FxOq7gSavz9e'   
+    }
+};
